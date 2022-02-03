74c7412fca7e88e63637faaed1469b7eb83d4ddb
Katie <galaxyventurer2000@gmail.com>
2022-02-02 21:01:35 -0700

    get the basic text box and a button

--- to-do/todo/src/App.js
+++ to-do/todo/src/App.js
@@ -1,16 +1,30 @@
+import React, {useState} from 'react'; 
 import './App.css';
 
 function App() {
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleUserInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const resetInputField = () => {
+    setInputValue("");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
           ToDo
       </header>
       <body className="Body">
-        hello
+        Enter new todo item:  
+        <input className="entryBox" type="text" value={inputValue} onChange={handleUserInput} />
+        <button className="addButton" onClick={resetInputField}>Add</button>
       </body>
       <footer className="Footer">
-        hello
+        Footer stuffs
       </footer>
     </div>
   );
