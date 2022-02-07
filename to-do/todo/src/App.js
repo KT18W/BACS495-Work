bdf89f86989b8b3a4c56f0cc2370a477d73345b7
Katie <galaxyventurer2000@gmail.com>
2022-02-07 15:10:15 -0700

    Todo app final

--- to-do/todo/src/App.js
+++ to-do/todo/src/App.js
@@ -3,28 +3,53 @@ import './App.css';
 
 function App() {
 
-  const [inputValue, setInputValue] = useState("");
+  const [value, setValue] = useState("");
 
-  const handleUserInput = (e) => {
-    setInputValue(e.target.value);
+  const submit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addItem(value);
+    setValue("");
   };
 
-  const resetInputField = () => {
-    setInputValue("");
+  const [items, setItems] = useState([]);
+
+  const addItem = text => {
+    const newItems = [...items, { text }];
+    setItems(newItems);
+  };
+
+  const markDone = index => {
+    const newItems = [...items];
+    newItems[index].isDone = true;
+    setItems(newItems);
   };
 
   return (
+
     <div className="App">
       <header className="App-header">
           ToDo
       </header>
       <body className="Body">
-        Enter new todo item:  
-        <input className="entryBox" type="text" value={inputValue} onChange={handleUserInput} />
-        <button className="addButton" onClick={resetInputField}>Add</button>
+        Add a new todo:
+        <form onSubmit={submit}>
+          <input className="entryBox" type="text" value={value} onChange={e => setValue(e.target.value)} />
+          <button onClick={submit}>Add</button>
+        </form>
       </body>
+      <div className="list">
+        {items.map((item, index) => (
+          <div className="done">
+            {!item.isDone ? (<img className="pic" src={require('./noCheck.jpg')} />) : ""}
+            {item.isDone ? (<img className="pic" src={require('./checkMark.jpg')} />) : ""}
+            {item.text}            
+            {!item.isDone ? (<button onClick={() => markDone(index)}>Mark as done</button>) : ""}
+          </div>
+        ))}
+      </div>
       <footer className="Footer">
-        Footer stuffs
+        2022
       </footer>
     </div>
   );
