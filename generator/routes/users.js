2d1fe42429b5379829a2b4c01cc43004ff2d5be1
Katie <galaxyventurer2000@gmail.com>
2022-02-18 15:14:55 -0700

    create the different methods

--- generator/routes/users.js
+++ generator/routes/users.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [
+  {"id":1, "name":"Joel"},
+  {"id":2, "name":"Katie"}
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(users);
+});
+
+router.get('/:id', function(req, res, next) {
+  var id = req.params.id;
+  console.log(id);
+  for(let user of users){
+    if(user.id == id){
+      res.json(user);
+    }
+  }
+  res.send("Cannot find user");
+});
+
+router.put('/:id', function(req, res, next) {
+  var id = req.params.id;
+  console.log(id);
+  for(let user of users){
+    if(user.id == id){
+      res.json(user);
+    }
+  }
+  res.send("Cannot find user");
+});
+
+router.post('/:id', function(req, res, next) {
+  var id = req.params.id;
+  console.log(id);
+  for(let user of users){
+    if(user.id == id){
+      res.json(user);
+    }
+  }
+  res.send("Cannot find user");
+});
+
+router.delete('/:id', function(req, res, next) {
+  var id = req.params.id;
+  console.log(id);
+  for(let user of users){
+    if(user.id == id){
+      res.json(user);
+    }
+  }
+  res.send("Cannot find user");
 });
 
 module.exports = router;
